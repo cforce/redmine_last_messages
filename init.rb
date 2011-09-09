e65bfc6b9139b3d6faaaca31c7ac0beb83850f9b
cforce <cforce@gmx.de>
2011-09-09 17:53:23 +0200

    Version raised

--- init.rb
+++ init.rb
@@ -5,5 +5,5 @@ Redmine::Plugin.register :redmine_last_messages do
   name 'Redmine last forum messages plugin'
   author 'Pavel Vinokurov, Sergei Vasiliev'
   description 'Show latest forum messages on Home and project Overview in a box similar to Latest News'
-  version '0.0.2'
+  version '0.0.3'
 end
